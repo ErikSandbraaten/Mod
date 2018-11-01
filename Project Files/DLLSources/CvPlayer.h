e86b5e5421f9f1d5aea8d418f9a240556714586d
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2018-11-01 16:15:20 +0100

    #204 Parallelized CvPlayerAI::AI_assignWorkingPlots and CvCityAI::AI_assignWorkingPlots. The first\outer level is roughly equivalent to parallel_for on all the player's cities, the second\nested\inner level is a reduction to determine the best job for a given unit.

--- Project Files/DLLSources/CvPlayer.h
+++ Project Files/DLLSources/CvPlayer.h
@@ -1116,6 +1116,10 @@ public:
 	void updateTransportThreshold(YieldTypes eYield);
 	// transport feeder - end - Nightinggale
 	void sortEuropeUnits();
+
+	// Clean this up
+	// TODO: Use the proper type
+	std::vector<int> m_validCityJobProfessions;
 };
 
 // cache CvPlayer::getYieldEquipmentAmount - start - Nightinggale
