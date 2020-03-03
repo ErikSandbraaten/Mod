99fc1c3a3e98afc1214cee82e53290d7b100d118
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2020-03-03 16:40:00 +0100

    #102 Work in progress, the AI handling is not yet functional. Based on a recent version of AdvCiv which includes a large amount of refactoring relative to WTP, hence the number of modified files.

--- Project Files/DLLSources/CvPlayer.cpp
+++ Project Files/DLLSources/CvPlayer.cpp
@@ -6678,7 +6678,7 @@ int CvPlayer::revolutionEuropeUnitThreshold() const
 
 CvPlot* CvPlayer::getStartingPlot() const
 {
-	return GC.getMapINLINE().plotSorenINLINE(m_iStartingX, m_iStartingY);
+	return GC.getMapINLINE().plotSoren(m_iStartingX, m_iStartingY);
 }
 
 
