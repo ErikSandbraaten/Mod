6f3efc1b3ac9759e39b06395dbd680aa17e6b619
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2020-03-03 16:40:00 +0100

    #102 Work in progress, the AI handling is not yet functional. Based on a recent version of AdvCiv which includes a large amount of refactoring relative to WTP, hence the number of modified files.

--- Project Files/DLLSources/CvCity.cpp
+++ Project Files/DLLSources/CvCity.cpp
@@ -3169,7 +3169,7 @@ bool CvCity::at(CvPlot* pPlot) const
 
 CvPlot* CvCity::plot() const
 {
-	return GC.getMapINLINE().plotSorenINLINE(getX_INLINE(), getY_INLINE());
+	return GC.getMapINLINE().plotSoren(getX_INLINE(), getY_INLINE());
 }
 
 int CvCity::getArea() const
@@ -3191,7 +3191,7 @@ CvArea* CvCity::waterArea() const
 
 CvPlot* CvCity::getRallyPlot() const
 {
-	return GC.getMapINLINE().plotSorenINLINE(m_iRallyX, m_iRallyY);
+	return GC.getMapINLINE().plotSoren(m_iRallyX, m_iRallyY);
 }
 
 
