58c05958fa6c9a27c30784c46eaeb2ee4b5fc0fd
Erik Sandbraaten <erik.sandbraaten@gmail.com>
2020-04-03 21:31:53 +0200

    #102 CvUnitAI::AI_respondToPickup : Fix bad evaluation
    Attempting to use getPathCost() here was clearly wrong since the path
    cost could be related to an unrelated pathfinding attempt!

--- Project Files/DLLSources/CvUnitAI.cpp
+++ Project Files/DLLSources/CvUnitAI.cpp
@@ -9261,6 +9261,8 @@ bool CvUnitAI::AI_respondToPickup(int iMaxPath, UnitAITypes eUnitAI)
 							}
 							// TAC - AI Improved Naval AI - koma13 - END
 
+							int iBestDirectionValue = MAX_INT;
+
 							if (bValid)
 							{
 								CvPlot* pLoopPlot = NULL;
@@ -9271,7 +9273,6 @@ bool CvUnitAI::AI_respondToPickup(int iMaxPath, UnitAITypes eUnitAI)
 								}
 								else
 								{
-									int iBestDirectionValue = MAX_INT;
 									for (int iDirection = 0; iDirection < NUM_DIRECTION_TYPES; iDirection++)
 									{
 										CvPlot* pDirectionPlot = plotDirection(pMissionPlot->getX_INLINE(), pMissionPlot->getY_INLINE(), (DirectionTypes)iDirection);
@@ -9305,7 +9306,7 @@ bool CvUnitAI::AI_respondToPickup(int iMaxPath, UnitAITypes eUnitAI)
 									}
 									
 									iValue *= 1000;
-									iValue /= 100 + ((iPathTurns == 0) ? 0 : getPathCost());
+									iValue /= 100 + ((iPathTurns == 0) ? 1 : iBestDirectionValue);
 									
 									int iDistance = stepDistance(pLoopSelectionGroup->getX(), pLoopSelectionGroup->getY(), pMissionPlot->getX_INLINE(), pMissionPlot->getY_INLINE());
 									
